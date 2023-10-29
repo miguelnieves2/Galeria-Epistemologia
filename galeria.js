ca520dbd88dfccccc0147270d115e3d984aaf013
Miguel <miguel.nieves246@gmail.com>
2023-10-29 03:42:56 -0500

    Primer commit

---
+++ galeria.js
@@ -0,0 +1,15 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+    controlArrows: true,
+    slidesNavigation: true,
+    navigation: false,
+    navigationTooltips: ['Sección 1', 'Sección 2', 'Sección 3', 'Sección 4'],
+    anchors: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    loopHorizontal: false,
+    fadingEffect: true,
+    lazyLoading: true,
+    keyboardScrolling: true,
+
+});
